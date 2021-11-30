bd058ca4e0806e0c64111280147d7e79ac200b46
lilgoode <christian.goode@bison.howard.edu>
2021-11-29 21:38:47 -0500

    sprint2

--- app/src/main/java/Fragments/HomeFragment.java
+++ app/src/main/java/Fragments/HomeFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +16,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.group.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import adapters.MovieAdapter;
+import models.Movie;
+import okhttp3.Headers;
+
+
+public class HomeFragment extends Fragment  {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=16e424c99e508208af70324ec7e25878";
+    public static final String TAG = "HomeFragment";
 
     private EditText etSearch;
     private Button btnSearch;
-    private ImageView ivMovie;
+
+
+
+    List<Movie> movies;
+
+
 
 
 
@@ -33,7 +57,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
 
     @Override
@@ -41,7 +68,46 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         etSearch = view.findViewById(R.id.etSearch);
         btnSearch = view.findViewById(R.id.btnSearch);
-        ivMovie = view.findViewById(R.id.ivMovie);
+
+        RecyclerView rvMovie = view.findViewById(R.id.rvMovie);
+        movies = new ArrayList<>();
+
+
+
+        MovieAdapter movieAdapter = new MovieAdapter(getContext(), movies);
+
+        rvMovie.setAdapter(movieAdapter);
+
+        rvMovie.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+
+            }
+
+        });
     }
-    //check
+                                                                           
 }
---
+++ app/src/main/java/models/Movie.java
@@ -0,0 +1,36 @@
+package models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+    }
+
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i ++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+
+    }
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
